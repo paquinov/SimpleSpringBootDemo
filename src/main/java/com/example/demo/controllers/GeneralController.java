7611e82844f830e353770befefff91a3c9826efe
Paul Aquino Vega <paquinov@ext.falabella.cl>
2023-01-09 21:29:19 -0500

    first commit

---
+++ src/main/java/com/example/demo/controllers/GeneralController.java
@@ -0,0 +1,40 @@
+package com.example.demo.controllers;
+
+import com.example.demo.dto.Product;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/demo")
+public class GeneralController {
+
+    @GetMapping("/products")
+    public ResponseEntity<List<Product>> getProducts() {
+        List<Product> products = Arrays.asList(Product.builder()
+                                                        .id(UUID.randomUUID().toString())
+                                                        .name("Yogurt")
+                                                        .description("Drink")
+                                                        .unitPrice(3.5)
+                                                        .build(),
+                                                Product.builder()
+                                                        .id(UUID.randomUUID().toString())
+                                                        .name("Donut")
+                                                        .description("Eatable")
+                                                        .unitPrice(1.7)
+                                                        .build(),
+                                                Product.builder()
+                                                        .id(UUID.randomUUID().toString())
+                                                        .name("TV")
+                                                        .description("Appliance")
+                                                        .unitPrice(350)
+                                                        .build());
+        return ResponseEntity.ok(products);
+    }
+
+}
---
+++ src/main/java/com/example/demo/dto/Product.java
@@ -0,0 +1,19 @@
+package com.example.demo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Product {
+
+    private String id;
+    private String name;
+    private String description;
+    private double unitPrice;
+
+}
