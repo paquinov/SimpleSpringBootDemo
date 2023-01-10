0a7107691bd6f06d6f8cc924e8d382e3e6a8c3e6
Paul Aquino Vega <paquinov@ext.falabella.cl>
2023-01-09 21:49:06 -0500

    Tests were fixed

--- src/main/java/com/example/demo/controllers/GeneralController.java
+++ src/main/java/com/example/demo/controllers/GeneralController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controllers;
 
 import com.example.demo.dto.Product;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/demo")
+@RequiredArgsConstructor
 public class GeneralController {
 
     @GetMapping("/products")
---
+++ src/test/java/com/example/demo/controllers/GeneralControllerTests.java
@@ -0,0 +1,28 @@
+package com.example.demo.controllers;
+
+import com.example.demo.dto.Product;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class GeneralControllerTests {
+
+    private GeneralController generalController;
+
+    @BeforeEach
+    void setup() {
+        generalController = new GeneralController();
+    }
+
+    @Test
+    void checkIfProductsAreNotEmpty() {
+        ResponseEntity<List<Product>> productsResponseEntity = generalController.getProducts();
+        Assertions.assertEquals(3, productsResponseEntity.getBody().size());
+    }
+
+}
